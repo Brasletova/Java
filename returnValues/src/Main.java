c8d6015bdd0cbd828fdbe8f7ab6b32586f905ae3
brasletova <brasletova@gmail.com>
2023-01-21 15:30:36 +0300

    Сreating a return value program

---
+++ returnValues/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = calculateSum(300, 200, 700);
+        System.out.println(sum);
+    }
+
+    public static int calculateSum(int a, int b, int c) {
+        int sum = a + b + c;
+        return sum;
+    }
+
+
+}
